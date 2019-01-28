46ba09d883d9db68dcc224496e1349e3345b50ee
Igor Tsallagov <43111727+igortsallagov@users.noreply.github.com>
2019-01-28 20:44:46 +0300

    Update assignment-01.py
    Modified function.

--- assignment-01.py
+++ assignment-01.py
@@ -1,26 +1,37 @@
-def adv_print(text, start='', max_line=120, in_file=False):
-    if in_file is True:
-        with open('adv_file.txt', 'w', encoding='UTF-8') as f:
-            f.write(text)
+def adv_print(*args, **kwargs):
+    if 'in_file' in kwargs.keys():
+        if kwargs['in_file'] is True:
+            with open('adv_file.txt', 'w', encoding='UTF-8') as f:
+                for arg in args:
+                    f.write(arg)
+    if 'max_line' in kwargs.keys():
+        max_line = kwargs['max_line']
+    else:
+        max_line = 120
+    if 'start' in kwargs.keys():
+        start = kwargs['start']
+    else:
+        start = ''
+    if 'sep' in kwargs.keys():
+        sep = kwargs['sep']
+    else:
+        sep = ' '
+    if 'end' in kwargs.keys():
+        end = kwargs['end']
+    else:
+        end = '\n'
+    if 'file' in kwargs.keys():
+        file = kwargs['file']
+    else:
+        file = None
+    if 'flush' in kwargs.keys():
+        flush = kwargs['flush']
+    else:
+        flush = False
+    line = sep.join([str(arg) for arg in args])
     i = 0
     step = max_line
-    while i < len(text):
-        print(f'{start}{text[i:max_line]}')
+    while i < len(line):
+        print(f'{start}{line[i:max_line]}', end=end, file=file, flush=flush)
         i += step
         max_line += step
-
-
-adv_print(text='The Count of Monte Cristo by Alexander Dumas')
-
-adv_print(text='The novel opens in 1815 as the Pharaon arrives in Marseille. The ship’s owner, Monsieur Morrel, '
-               'learns from the young first mate, Edmond Dantès, that the captain died on the journey and that Dantès '
-               'took over. The ship’s accountant, Danglars, is bothered that the Pharaon stopped at Elba, but Dantès '
-               'explains that the captain left a package to be delivered to one of Napoleon’s marshals who is in '
-               'exile with Napoleon on the island. Morrel makes Dantès captain of the ship, to Danglars’s '
-               'displeasure. On visiting his father, Dantès learns that a neighbour, Gaspard Caderousse, took most of '
-               'his father’s resources in payment of a debt. Dantès then goes to see his fiancée, Mercédès, '
-               'and finds her in the company of Fernand Mondego, who is in love with her. After leaving, '
-               'Mondego encounters Danglars and Caderousse, and a decision is made to falsely accuse Dantès of '
-               'treason. In a letter to the crown prosecutor, Danglars alleges that Dantès is a Bonapartist and is '
-               'carrying a letter from Napoleon to the Bonapartist committee in Paris.', start='^', in_file=True,
-          max_line=90)
